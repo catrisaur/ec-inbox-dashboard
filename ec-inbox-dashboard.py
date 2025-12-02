f746a47b3d9ee82f1ee222a3fcf193d7fcc91e71
Paige I. <150849500+catrisaur@users.noreply.github.com>
2025-12-02 11:40:19 +0800

    no need wordcloud

--- ec-inbox-dashboard.py
+++ ec-inbox-dashboard.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # =========================================================
